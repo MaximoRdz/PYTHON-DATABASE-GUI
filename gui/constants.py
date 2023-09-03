70b9ff9408731b4e401f8e03ff42dc5e22593de8
MaximoRdz <maximo.rodriguez@estudiante.uam.es>
2023-09-03 12:45:02 +0200

    Add Collectors Max Number

--- gui/constants.py
+++ gui/constants.py
@@ -1,4 +1,5 @@
 FONTSIZE_TEXT = 17
 FONTSIZE_HEADER = 17
-TABLE_WIDGET_ENTRIES = 10       # Number of rows shown in the gui.
-DESCRIPTION_DISPLAY_LEN = 50    # Maximum length of the text shown in the gui.
+TABLE_WIDGET_ENTRIES = 10  # Number of rows shown in the gui.
+DESCRIPTION_DISPLAY_LEN = 50  # Maximum length of the text shown in the gui.
+MAXIMUM_COLLECTOR_VALUES = 10  # Maximum number of collector allowed to save.
